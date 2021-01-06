56880a9c8ded0b9329ff8a432dc0b456b46c67c0
InFinity_54 <corentinkreicher@gmail.com>
2021-01-06 09:03:14 +0100

    Ajout du projet généré par Spring Initializr

---
+++ src/test/java/fr/infinity54/cesi/advancedjava/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.infinity54.cesi.advancedjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
